2d772084342f3ad08dd68fd14b609987427e03bb
Mohamed Mosalm <mohamed.mosalm2002@gmail.com>
2023-12-26 08:38:50 +0200

    created user model

---
+++ models/userModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'A user email is required'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'please enter a valid email'],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'A user password is required'],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirmation: {
+    type: String,
+    required: [true, 'A user password confirmation is required'],
+    validate: {
+      validator: function (value) {
+        return value === this.password;
+      },
+      message: `password confirmation doesn't match password`,
+    },
+    select: false,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  this.passwordConfirmation = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  reqPassword,
+  userPassword,
+) {
+  return await bcrypt.compare(reqPassword, userPassword);
+};
+
+module.exports = mongoose.model('User', userSchema);
